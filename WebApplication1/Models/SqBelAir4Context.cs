fbf65f1fd9178744dc70764c3b10887420ac0442
Gail Dausin <johnpua.jmp@gmail.cpm>
2026-05-21 14:20:18 +0800

    Installation

--- WebApplication1/Models/SqBelAir4Context.cs
+++ WebApplication1/Models/SqBelAir4Context.cs
@@ -25,7 +25,7 @@ public partial class SqBelAir4Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-SV4QTVS;Database=SqBelAir4;User Id=sa;Password=P@ssw0rdSqL;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=SqBelAir4;User Id=sa;Password=P@ssw0rdSqL;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
